d392059256a90a64274d2eb29c33d3b1ca68351b
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-02-25 10:59:25 +0100

    Første commit

---
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/BidragBidragskalkulatorApiApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.bidrag.bidragskalkulator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BidragBidragskalkulatorApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<BidragBidragskalkulatorApiApplication>(*args)
+}
---
+++ src/test/kotlin/no/nav/bidrag/bidragskalkulator/BidragBidragskalkulatorApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.bidrag.bidragskalkulator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BidragBidragskalkulatorApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
