bd77543a05464d6e2f5baf88c029715bc4338a6b
natchananthawisit <natcha.nanthawisit@nav.no>
2025-03-10 14:42:39 +0100

    Fjern springboottest i filene som ikke er test

--- src/test/kotlin/no/nav/bidrag/bidragskalkulator/BidragBidragskalkulatorApiApplicationTests.kt
+++ src/test/kotlin/no/nav/bidrag/bidragskalkulator/BidragBidragskalkulatorApiApplicationTests.kt
@@ -1,13 +1,5 @@
 package no.nav.bidrag.bidragskalkulator
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class BidragBidragskalkulatorApiApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
 }
