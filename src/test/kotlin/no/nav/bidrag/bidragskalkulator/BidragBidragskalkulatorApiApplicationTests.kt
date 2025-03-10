32459e2f2526b6786dd377805fd270ebb70c2315
natchananthawisit <natcha.nanthawisit@nav.no>
2025-03-10 15:08:19 +0100

    La til spring profil eksplisitt

--- src/test/kotlin/no/nav/bidrag/bidragskalkulator/BidragBidragskalkulatorApiApplicationTests.kt
+++ src/test/kotlin/no/nav/bidrag/bidragskalkulator/BidragBidragskalkulatorApiApplicationTests.kt
@@ -1,8 +1,16 @@
 package no.nav.bidrag.bidragskalkulator
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class])
 class BidragBidragskalkulatorApiApplicationTests
+
+
+fun main(args: Array<String>) {
+    val app = SpringApplication(BidragBidragskalkulatorApiApplication::class.java)
+    app.setAdditionalProfiles("test")
+    app.run(*args)
+}
