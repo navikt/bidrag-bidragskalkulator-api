20cceb579567f3b9cb2084972af1bc2921133d5b
natchananthawisit <natcha.nanthawisit@nav.no>
2025-03-13 11:18:47 +0100

    Hent underholdskostnad fra beregningsmodullen

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/service/BeregningService.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/service/BeregningService.kt
@@ -1,12 +1,16 @@
 package no.nav.bidrag.bidragskalkulator.service
 
 import no.nav.bidrag.beregn.barnebidrag.BeregnBarnebidragApi
-import no.nav.bidrag.bidragskalkulator.dto.BeregningsresultatDto
 import no.nav.bidrag.bidragskalkulator.dto.BeregningRequestDto
 import no.nav.bidrag.bidragskalkulator.dto.BeregningsresultatBarnDto
+import no.nav.bidrag.bidragskalkulator.dto.BeregningsresultatDto
 import no.nav.bidrag.bidragskalkulator.mapper.BeregningsgrunnlagMapper
+import no.nav.bidrag.domene.enums.grunnlag.Grunnlagstype
+import no.nav.bidrag.transport.behandling.beregning.felles.BeregnGrunnlag
+import no.nav.bidrag.transport.behandling.felles.grunnlag.*
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
+
 
 @Service
 class BeregningService(
@@ -18,14 +22,23 @@ class BeregningService(
         val beregningsgrunnlag = beregningsgrunnlagMapper.mapTilBeregningsgrunnlag(beregningRequest)
 
         val beregningsresultat = beregningsgrunnlag.map { data ->
+
             BeregningsresultatBarnDto(
                 sum = beregnBarnebidragApi.beregn(data.grunnlag)
                     .beregnetBarnebidragPeriodeListe
                     .sumOf { it.resultat.beløp ?: BigDecimal.ZERO },
-                barnetsAlder = data.barnetsAlder
+                barnetsAlder = data.barnetsAlder,
+                underholdskostnad = hentUnderholdskostnad(data.grunnlag)
             )
         }
 
         return BeregningsresultatDto(beregningsresultat)
     }
+
+    internal fun hentUnderholdskostnad(grunnlag: BeregnGrunnlag): BigDecimal =
+        beregnBarnebidragApi.beregnUnderholdskostnad(grunnlag)
+            .firstOrNull { it.type == Grunnlagstype.DELBEREGNING_UNDERHOLDSKOSTNAD }
+            ?.innholdTilObjekt<DelberegningUnderholdskostnad>()
+            ?.underholdskostnad
+            ?: throw NoSuchElementException("Ingen underholdskostnad funnet i beregningen")
 }
