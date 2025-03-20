7781801920a4e08d9477afacd753d44c18501aae
natchananthawisit <natcha.nanthawisit@nav.no>
2025-03-20 10:19:47 +0100

    Gi tilgang til token generator service

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/controller/BeregningController.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/controller/BeregningController.kt
@@ -7,10 +7,12 @@ import no.nav.bidrag.bidragskalkulator.dto.BeregningsresultatDto
 import no.nav.bidrag.bidragskalkulator.dto.BeregningRequestDto
 import jakarta.validation.Valid
 import no.nav.bidrag.bidragskalkulator.service.BeregningService
+import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/beregning")
+@ProtectedWithClaims(issuer = "tokenx")
 class BeregningController(private val beregningService: BeregningService) {
 
     @Operation(summary = "Beregner barnebidrag", description = "Beregner barnebidrag basert på inntekten til foreldre og barnets alder. Returnerer 200 ved vellykket beregning.")
