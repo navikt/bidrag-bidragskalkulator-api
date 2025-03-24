4aef2c05c3ed4cd7fc5a7223ff1c1c4804e58e90
natchananthawisit <natcha.nanthawisit@nav.no>
2025-03-24 08:49:44 +0100

    Konfigurert JWT og OpenApi

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/config/OpenApiConfig.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/config/OpenApiConfig.kt
@@ -1,12 +1,33 @@
-import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.media.Schema
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.media.Schema
+import io.swagger.v3.oas.models.security.SecurityScheme
+import no.nav.bidrag.commons.security.api.EnableSecurityConfiguration
+import no.nav.bidrag.domene.enums.beregning.Samværsklasse
 import org.springdoc.core.customizers.OpenApiCustomizer
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import no.nav.bidrag.domene.enums.beregning.Samværsklasse
+import org.springframework.retry.annotation.EnableRetry
 
 @Configuration
+@EntityScan("no.nav.bidrag.bidragskalkulator.dto")
+@io.swagger.v3.oas.annotations.security.SecurityScheme(
+    bearerFormat = "JWT",
+    name = "bearer-key",
+    scheme = "bearer",
+    type = SecuritySchemeType.HTTP
+)
+@OpenAPIDefinition(
+    info = io.swagger.v3.oas.annotations.info.Info(title = "bidrag-bidragskalkulator-api", version = "v1"),
+    security = [SecurityRequirement(name = "bearer-key")]
+)
+@EnableSecurityConfiguration
+@EnableRetry
 class OpenApiConfig {
 
     @Bean
@@ -22,5 +43,17 @@ class OpenApiConfig {
             openApi.components = (openApi.components ?: Components())
                 .addSchemas("Samværsklasse", samværsklasseSchema)
         }
+    }
+
+    @Bean
+    fun openAPI(): OpenAPI {
+        return OpenAPI()
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        "bearer-key",
+                        SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                    )
+            ).info(Info().title("bidrag-bidragskalkulator-api").version("v1"))
     }
 }
--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/controller/BeregningController.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/controller/BeregningController.kt
@@ -3,19 +3,26 @@ package no.nav.bidrag.bidragskalkulator.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import no.nav.bidrag.bidragskalkulator.dto.BeregningsresultatDto
 import no.nav.bidrag.bidragskalkulator.dto.BeregningRequestDto
 import jakarta.validation.Valid
 import no.nav.bidrag.bidragskalkulator.service.BeregningService
+import no.nav.security.token.support.core.api.Protected
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/beregning")
 @ProtectedWithClaims(issuer = "tokenx")
+@Validated
 class BeregningController(private val beregningService: BeregningService) {
 
-    @Operation(summary = "Beregner barnebidrag", description = "Beregner barnebidrag basert på inntekten til foreldre og barnets alder. Returnerer 200 ved vellykket beregning.")
+    @Protected
+    @Operation(summary = "Beregner barnebidrag",
+        description = "Beregner barnebidrag basert på inntekten til foreldre og barnets alder. Returnerer 200 ved vellykket beregning.",
+        security = arrayOf(SecurityRequirement(name = "bearer-key")))
     @ApiResponses(
         value = [
             ApiResponse(responseCode = "200", description = "Beregning fullført"),
