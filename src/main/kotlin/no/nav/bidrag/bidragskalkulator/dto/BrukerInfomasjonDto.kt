f969e27052abb9ef99aa1c77a91b67ea298b34f3
natchananthawisit <natcha.nanthawisit@nav.no>
2025-04-10 13:41:36 +0200

    En ny dto for brukerinformasjon med barn-relasjoner

---
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/dto/BrukerInfomasjonDto.kt
@@ -0,0 +1,36 @@
+package no.nav.bidrag.bidragskalkulator.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "Inneholder informasjon om en person")
+data class PersonDto(
+    @Schema(description = "Unik identifikator for personen (fødselsnummer eller D-nummer)", example = "12345678901")
+    val ident: String,
+
+    @Schema(description = "Fornavn til personen", example = "Ola")
+    val fornavn: String,
+
+    @Schema(description = "Visningsnavn, som er fornavn og etternavn kombinert", example = "Ola Nordmann")
+    val visningsnavn: String,
+
+    @Schema(description = "Alder til personen", example = "12",)
+    val alder: Int
+)
+
+@Schema(description = "Representerer en foreldre-barn-relasjon, med felles barn og motpart")
+data class BarneRelasjon(
+    @Schema(description = "Motparten i relasjonen, vanligvis den andre forelderen")
+    val motpart: PersonDto,
+
+    @Schema(description = "Liste over felles barn mellom pålogget person og motparten")
+    val fellesBarn: List<PersonDto>
+)
+
+@Schema(description = "Informasjon om pålogget person og relasjoner til barn")
+data class BrukerInfomasjonDto(
+    @Schema(description = "Informasjon om pålogget person")
+    val paaloggetPerson: PersonDto,
+
+    @Schema(description = "Liste over barn til pålogget person, gruppert med motpart")
+    val barnRelasjon: List<BarneRelasjon>
+)
