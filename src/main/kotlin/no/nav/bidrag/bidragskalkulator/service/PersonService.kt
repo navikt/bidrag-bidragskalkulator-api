778f45920b51740cddaca48e2809b4419d0cbf94
Ugur Alpay Cenar <cenar93@gmail.com>
2025-04-22 16:53:13 +0200

    Bruk av tokenx ved kall mot eksterne tjenester

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/service/PersonService.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/service/PersonService.kt
@@ -3,13 +3,16 @@ package no.nav.bidrag.bidragskalkulator.service
 import no.nav.bidrag.bidragskalkulator.consumer.BidragPersonConsumer
 import no.nav.bidrag.bidragskalkulator.dto.BrukerInfomasjonDto
 import no.nav.bidrag.bidragskalkulator.mapper.BrukerInformasjonMapper
+import no.nav.bidrag.commons.security.SikkerhetsKontekst
 import org.springframework.stereotype.Service
 
 @Service
 class PersonService(private val personConsumer: BidragPersonConsumer) {
 
     fun hentInformasjon(personIdent: String): BrukerInfomasjonDto {
-        val familierelasjon = personConsumer.hentFamilierelasjon(personIdent)
+        val familierelasjon =  SikkerhetsKontekst.medApplikasjonKontekst {
+            personConsumer.hentFamilierelasjon(personIdent)
+        }
 
         return BrukerInformasjonMapper.tilBrukerInformasjonDto(familierelasjon)
     }
