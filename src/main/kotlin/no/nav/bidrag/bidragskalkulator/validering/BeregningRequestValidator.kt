5acb906d4aa7108c3dff679ee5bc663b4dc086e7
natchananthawisit <natcha.nanthawisit@nav.no>
2025-05-20 09:52:48 +0200

    Fikset feil variabelsnavn

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/validering/BeregningRequestValidator.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/validering/BeregningRequestValidator.kt
@@ -10,13 +10,13 @@ object BeregningRequestValidator {
         val harPliktigeBarn = dto.barn.any { it.bidragstype == PLIKTIG }
         val harMottakerBarn = dto.barn.any { it.bidragstype == MOTTAKER }
 
-        val manglerDinBoforhold = harPliktigeBarn && dto.dittBoforhold == null
+        val manglerDittBoforhold = harPliktigeBarn && dto.dittBoforhold == null
         val manglerMedforelderBoforhold = harMottakerBarn && dto.medforelderBoforhold == null
 
         when {
-            manglerDinBoforhold && manglerMedforelderBoforhold ->
+            manglerDittBoforhold && manglerMedforelderBoforhold ->
                 feil("Både 'dittBoforhold' og 'medforelderBoforhold' mangler, men må være satt når forespørselen inneholder barn der du er bidragspliktig og/eller bidragsmottaker.")
-            manglerDinBoforhold ->
+            manglerDittBoforhold ->
                 feil("'dittBoforhold' må være satt fordi forespørselen inneholder minst ett barn der du er bidragspliktig.")
             manglerMedforelderBoforhold ->
                 feil("'medforelderBoforhold' må være satt fordi forespørselen inneholder minst ett barn der du er bidragsmottaker.")
