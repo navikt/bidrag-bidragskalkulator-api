798295619d7db7697ae4fdf79f7bd7595545ec67
natchananthawisit <natcha.nanthawisit@nav.no>
2025-05-21 16:11:46 +0200

    Oppdatert BrukerInformasjonMapper til å inkludere underholdskostnad

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/mapper/BrukerInformasjonMapper.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/mapper/BrukerInformasjonMapper.kt
@@ -1,12 +1,14 @@
 package no.nav.bidrag.bidragskalkulator.mapper
 
 import no.nav.bidrag.bidragskalkulator.dto.*
+import no.nav.bidrag.bidragskalkulator.service.BarnUnderholdskostnad
 import no.nav.bidrag.bidragskalkulator.utils.kalkulereAlder
 import no.nav.bidrag.domene.enums.person.Diskresjonskode
 import no.nav.bidrag.transport.behandling.inntekt.response.TransformerInntekterResponse
 import no.nav.bidrag.transport.person.MotpartBarnRelasjonDto
 import no.nav.bidrag.transport.person.PersonDto
 import org.slf4j.LoggerFactory
+import java.math.BigDecimal
 
 
 private var FORTROLIG_ADRESSE_DISKRESJONSKODER = listOf(
@@ -21,6 +23,7 @@ object BrukerInformasjonMapper {
 
     fun tilBrukerInformasjonDto(
         motpartBarnRelasjondto: MotpartBarnRelasjonDto,
+        barnUnderholdkostnad:  List<BarnUnderholdskostnad>,
         inntektsGrunnlag: TransformerInntekterResponse?
     ): BrukerInformasjonDto {
 
@@ -45,8 +48,10 @@ object BrukerInformasjonMapper {
                         fellesBarn = it.fellesBarn
                             .filterNot { barn -> barn.erDød() }
                             .filterNot { barn -> barn.harFortroligAdresse() }
-                            .map { barn -> barn.tilPersonInformasjonDto() }
-                            .sortedByDescending { barn -> barn.alder }
+                            .map { barn ->
+                                val barnetUnderholdskostnad = barnUnderholdkostnad.find { it.barnIdent == barn.ident}?.underholdskostnad
+                                barn.tilBarnInformasjonDto(barnetUnderholdskostnad)
+                            }.sortedByDescending { barn -> barn.alder }
                     )
                 }
                 .filterNot { it.fellesBarn.isEmpty() }
@@ -66,6 +71,16 @@ object BrukerInformasjonMapper {
             ident = this.ident,
             fornavn = this.fornavn ?: "",
             fulltNavn = this.visningsnavn,
+            alder = this.fødselsdato?.let { kalkulereAlder(it) } ?: 0
+        )
+    }
+
+    private fun PersonDto.tilBarnInformasjonDto(underholdskostnad: BigDecimal?): BarnInformasjonDto {
+        return BarnInformasjonDto(
+            ident = this.ident,
+            fornavn = this.fornavn ?: "",
+            fulltNavn = this.visningsnavn,
+            underholdskostnad = underholdskostnad ?: BigDecimal.ZERO,
             alder = this.fødselsdato?.let { kalkulereAlder(it) } ?: 0
         )
     }
