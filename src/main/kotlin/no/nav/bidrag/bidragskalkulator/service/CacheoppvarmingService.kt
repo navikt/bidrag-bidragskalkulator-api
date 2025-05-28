d4311e5b7595582b5284b824f81837709af9cb9f
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-05-28 11:34:31 +0200

    Ikke kjør cache-oppvarming mens man tester

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/service/CacheoppvarmingService.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/service/CacheoppvarmingService.kt
@@ -2,9 +2,12 @@ package no.nav.bidrag.bidragskalkulator.service
 
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
+@Profile("!test")
 @Component
+
 class CacheoppvarmingService(
     private val underholdskostnadService: UnderholdskostnadService
 ) : ApplicationRunner {
