2c1887fe02934a4ee0a9ccc575ee9f4b02762113
Kristian Storvoll <kristian.storvoll@nav.no>
2025-06-17 09:43:28 +0200

    Endre GET til POST

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/controller/PrivatAvtaleController.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/controller/PrivatAvtaleController.kt
@@ -14,6 +14,7 @@ import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -24,8 +25,7 @@ import org.springframework.web.bind.annotation.RestController
 class PrivatAvtaleController(private val privatAvtaleService: PrivatAvtalePdfService) {
 
 
-
-    @GetMapping(produces = [MediaType.APPLICATION_PDF_VALUE])
+    @PostMapping(produces = [MediaType.APPLICATION_PDF_VALUE])
     @Operation(
         summary = "Generer privat avtale PDF",
         description = "Genererer en privat avtale i PDF-format.",
