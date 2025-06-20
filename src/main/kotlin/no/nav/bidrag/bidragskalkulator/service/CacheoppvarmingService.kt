42a3f085202826d54650aee5ed58abc24c1f1993
natchananthawisit <natcha.nanthawisit@nav.no>
2025-06-20 12:18:32 +0200

    Disabled chacheoppvarming for test profil

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/service/CacheoppvarmingService.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/service/CacheoppvarmingService.kt
@@ -2,9 +2,11 @@ package no.nav.bidrag.bidragskalkulator.service
 
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
 @Component
+@Profile("!test")
 class CacheoppvarmingService(
     private val underholdskostnadService: UnderholdskostnadService,
     private val sjablonService: SjablonService
