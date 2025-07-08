5844b34fcc4a1630b3b166ecd6fb069010398669
Kristian Storvoll <kristian.storvoll@nav.no>
2025-07-08 09:59:22 +0200

    Tests

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/consumer/SafSelvbetjeningConsumer.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/consumer/SafSelvbetjeningConsumer.kt
@@ -6,9 +6,11 @@ import no.nav.bidrag.bidragskalkulator.config.SafSelvbetjeningConfigurationPrope
 import no.nav.bidrag.bidragskalkulator.dto.SafSelvbetjeningResponsDto
 import no.nav.bidrag.commons.util.secureLogger
 import no.nav.bidrag.commons.web.client.AbstractRestClient
-import org.springframework.http.HttpEntity
+import org.apache.hc.core5.http.HttpException
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
@@ -16,8 +18,8 @@ import java.net.URI
 
 
 class SafSelvbetjeningConsumer(
-    private val properties: SafSelvbetjeningConfigurationProperties,
-    private val restTemplate: RestTemplate,
+    properties: SafSelvbetjeningConfigurationProperties,
+    restTemplate: RestTemplate,
 ) : AbstractRestClient(restTemplate, "saf.selvbetjening") {
 
     private val url = UriComponentsBuilder.fromUri(URI.create(properties.graphqlUrl))
@@ -29,8 +31,7 @@ class SafSelvbetjeningConsumer(
         ident: String, tema: List<String> = listOf("BID")
     ): SafSelvbetjeningResponsDto? {
 
-
-        val document = """
+        val query = """
               {
                 dokumentoversiktSelvbetjening(ident: "$ident", tema: [BID]) {
                   journalposter {
@@ -61,262 +62,25 @@ class SafSelvbetjeningConsumer(
             contentType = MediaType.APPLICATION_JSON
         }
 
-        val requestBody = GraphQLRequest(document,  mapOf("ident" to ident, "tema" to tema))
+        val requestBody = GraphQLRequest(query,  mapOf())
         try {
-            val string = postForNonNullEntity<String>(url, requestBody, headers)
-            val mapping = ObjectMapper().registerKotlinModule().readValue(string, SafSelvbetjeningResponsDto::class.java)
-            return mapping
-        } catch (e: RestClientException) {
-            secureLogger.error {"Feil ved henting av dokumenter for bruker: ${e.message}" }
+            return postForNonNullEntity<SafSelvbetjeningResponsDto>(url, requestBody, headers)
+        } catch (e: HttpClientErrorException) {
+            when (e.statusCode.value()) {
+                404 -> {
+                    secureLogger.error { "Tilgang feilet ved henting av dokumenter: ${e.message}" }
+                    throw HttpClientErrorException(HttpStatus.UNAUTHORIZED, "Tilgang feilet ved henting av dokumenter")
+                }
+                else -> {
+                    secureLogger.error {"Feil ved henting av dokumenter for bruker: ${e.message}" }
+                    throw RuntimeException("Kunne ikke hente dokumenter", e)
+                }
+            }
+        } catch (e: Exception) {
+            secureLogger.error { "Uventet feil ved henting av dokumenter for bruker: ${e.message}" }
             throw RuntimeException("Kunne ikke hente dokumenter", e)
         }
     }
-
-
-    val mockResponse = """
-        {
-          "data": {
-            "dokumentoversiktSelvbetjening": {
-              "journalposter": [
-                {
-                  "tema": "BID",
-                  "tittel": "INNGÅENDE osadasdasm barnebidrag (uten dokumentdato)",
-                  "datoSortering": "2022-02-09T12:11:13",
-                  "journalpostId": "454000114",
-                  "mottaker": null,
-                  "avsender": {
-                    "navn": "Hansen, Per"
-                  },
-                  "dokumenter": [
-                    {
-                      "dokumentInfoId": "454408109",
-                      "tittel": "Vedtak om barnebidrag (test)",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    }
-                  ]
-                },
-                {
-                  "tema": "BID",
-                  "tittel": "TEST2 osadasdasm barnebidrag (uten dokumentdato)",
-                  "datoSortering": "2022-02-09T12:11:13",
-                  "journalpostId": "454000113",
-                  "mottaker": null,
-                  "avsender": {
-                    "navn": "Hansen, Per"
-                  },
-                  "dokumenter": [
-                    {
-                      "dokumentInfoId": "454408108",
-                      "tittel": "Vedtak om barnebidrag (test)",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    }
-                  ]
-                },
-                {
-                  "tema": "BID",
-                  "tittel": "TEST osadasdasm barnebidrag (uten dokumentdato)",
-                  "datoSortering": "2025-07-01T13:15:41",
-                  "journalpostId": "454000106",
-                  "mottaker": {
-                    "navn": "Hansen, Per"
-                  },
-                  "avsender": null,
-                  "dokumenter": [
-                    {
-                      "dokumentInfoId": "454408101",
-                      "tittel": "Vedtak om barnebidrag (test)",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    }
-                  ]
-                },
-                {
-                  "tema": "BID",
-                  "tittel": "Vedtak barnebidrag",
-                  "datoSortering": "2025-07-01T13:07:13",
-                  "journalpostId": "454000102",
-                  "mottaker": {
-                    "navn": "Selvhjulpen Mandag"
-                  },
-                  "avsender": null,
-                  "dokumenter": [
-                    {
-                      "dokumentInfoId": "454408093",
-                      "tittel": "Vedtak barnebidrag",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    },
-                    {
-                      "dokumentInfoId": "454408094",
-                      "tittel": "Vedtak barnebidrag",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    },
-                    {
-                      "dokumentInfoId": "454408095",
-                      "tittel": "Information to the custodial parent when one of the parties is living abroad (Vedlegg til vedtak bidragsmottaker)",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    },
-                    {
-                      "dokumentInfoId": "454408096",
-                      "tittel": "Antwortformular für Unterhaltspflichtige (Svarskjema bidragspliktig)",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    },
-                    {
-                      "dokumentInfoId": "454408097",
-                      "tittel": "Bidragsforskudd, Saksbehandlingsnotat",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    }
-                  ]
-                },
-                {
-                  "tema": "BID",
-                  "tittel": "Vedtak barnebidrag",
-                  "datoSortering": "2025-06-27T12:43:26",
-                  "journalpostId": "453999638",
-                  "mottaker": {
-                    "navn": "Selvhjulpen Mandag"
-                  },
-                  "avsender": null,
-                  "dokumenter": [
-                    {
-                      "dokumentInfoId": "454407595",
-                      "tittel": "Vedtak barnebidrag",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    }
-                  ]
-                },
-                {
-                  "tema": "BID",
-                  "tittel": "Barnebidrag, Saksbehandlingsnotat (fra bidragsmottaker sak 2500056)",
-                  "datoSortering": "2025-06-27T09:11:46",
-                  "journalpostId": "453999556",
-                  "mottaker": {
-                    "navn": "Selvhjulpen Mandag"
-                  },
-                  "avsender": null,
-                  "dokumenter": [
-                    {
-                      "dokumentInfoId": "454407507",
-                      "tittel": "Barnebidrag, Saksbehandlingsnotat (fra bidragsmottaker sak 2500056)",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    }
-                  ]
-                },
-                {
-                  "tema": "BID",
-                  "tittel": "Svar på forhåndsvarsel i sak om barnebidrag til bidragsmottaker",
-                  "datoSortering": "2025-06-24T11:17:17",
-                  "journalpostId": "453998552",
-                  "mottaker": {
-                    "navn": "Selvhjulpen Mandag"
-                  },
-                  "avsender": null,
-                  "dokumenter": [
-                    {
-                      "dokumentInfoId": "454406332",
-                      "tittel": "Svar på forhåndsvarsel i sak om barnebidrag til bidragsmottaker",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    }
-                  ]
-                },
-                {
-                  "tema": "BID",
-                  "tittel": "Bidragsforskudd, Saksbehandlingsnotat",
-                  "datoSortering": "2025-06-24T09:24:44",
-                  "journalpostId": "453998503",
-                  "mottaker": {
-                    "navn": "Selvhjulpen Mandag"
-                  },
-                  "avsender": null,
-                  "dokumenter": [
-                    {
-                      "dokumentInfoId": "454406259",
-                      "tittel": "Bidragsforskudd, Saksbehandlingsnotat",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    },
-                    {
-                      "dokumentInfoId": "454406260",
-                      "tittel": "WNIOSEK O DORĘCZENIE ZA GRANICĄ DOKUMENTÓW SĄDOWYCH I POZASĄDOWYCH (Forkynningsskjema (Polsk, Fransk, Engelsk))",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    }
-                  ]
-                },
-                {
-                  "tema": "BID",
-                  "tittel": "Søknadsskjema for bidragspliktig",
-                  "datoSortering": "2025-03-09T10:17:46",
-                  "journalpostId": "453969746",
-                  "mottaker": null,
-                  "avsender": {
-                    "navn": "SELVHJULPEN, MANDAG"
-                  },
-                  "dokumenter": [
-                    {
-                      "dokumentInfoId": "454373261",
-                      "tittel": "Søknadsskjema for bidragspliktig",
-                      "dokumentvarianter": [
-                        {
-                          "variantformat": "ARKIV"
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
-            }
-          }
-        }
-    """.trimIndent()
 
 }
 
--- src/test/kotlin/no/nav/bidrag/bidragskalkulator/consumer/SafSelvbetjeningConsumerTest.kt
+++ src/test/kotlin/no/nav/bidrag/bidragskalkulator/consumer/SafSelvbetjeningConsumerTest.kt
@@ -6,9 +6,253 @@ import org.junit.jupiter.api.BeforeEach
 
 class SafSelvbetjeningConsumerTest {
 
-
     @BeforeEach
     fun setUp() {
         // Initialize any necessary components or mocks here
     }
 }
+
+val mockResponse = """
+        {
+          "data": {
+            "dokumentoversiktSelvbetjening": {
+              "journalposter": [
+                {
+                  "tema": "BID",
+                  "tittel": "INNGÅENDE osadasdasm barnebidrag (uten dokumentdato)",
+                  "datoSortering": "2022-02-09T12:11:13",
+                  "journalpostId": "454000114",
+                  "mottaker": null,
+                  "avsender": {
+                    "navn": "Hansen, Per"
+                  },
+                  "dokumenter": [
+                    {
+                      "dokumentInfoId": "454408109",
+                      "tittel": "Vedtak om barnebidrag (test)",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "tema": "BID",
+                  "tittel": "TEST2 osadasdasm barnebidrag (uten dokumentdato)",
+                  "datoSortering": "2022-02-09T12:11:13",
+                  "journalpostId": "454000113",
+                  "mottaker": null,
+                  "avsender": {
+                    "navn": "Hansen, Per"
+                  },
+                  "dokumenter": [
+                    {
+                      "dokumentInfoId": "454408108",
+                      "tittel": "Vedtak om barnebidrag (test)",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "tema": "BID",
+                  "tittel": "TEST osadasdasm barnebidrag (uten dokumentdato)",
+                  "datoSortering": "2025-07-01T13:15:41",
+                  "journalpostId": "454000106",
+                  "mottaker": {
+                    "navn": "Hansen, Per"
+                  },
+                  "avsender": null,
+                  "dokumenter": [
+                    {
+                      "dokumentInfoId": "454408101",
+                      "tittel": "Vedtak om barnebidrag (test)",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "tema": "BID",
+                  "tittel": "Vedtak barnebidrag",
+                  "datoSortering": "2025-07-01T13:07:13",
+                  "journalpostId": "454000102",
+                  "mottaker": {
+                    "navn": "Selvhjulpen Mandag"
+                  },
+                  "avsender": null,
+                  "dokumenter": [
+                    {
+                      "dokumentInfoId": "454408093",
+                      "tittel": "Vedtak barnebidrag",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    },
+                    {
+                      "dokumentInfoId": "454408094",
+                      "tittel": "Vedtak barnebidrag",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    },
+                    {
+                      "dokumentInfoId": "454408095",
+                      "tittel": "Information to the custodial parent when one of the parties is living abroad (Vedlegg til vedtak bidragsmottaker)",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    },
+                    {
+                      "dokumentInfoId": "454408096",
+                      "tittel": "Antwortformular für Unterhaltspflichtige (Svarskjema bidragspliktig)",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    },
+                    {
+                      "dokumentInfoId": "454408097",
+                      "tittel": "Bidragsforskudd, Saksbehandlingsnotat",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "tema": "BID",
+                  "tittel": "Vedtak barnebidrag",
+                  "datoSortering": "2025-06-27T12:43:26",
+                  "journalpostId": "453999638",
+                  "mottaker": {
+                    "navn": "Selvhjulpen Mandag"
+                  },
+                  "avsender": null,
+                  "dokumenter": [
+                    {
+                      "dokumentInfoId": "454407595",
+                      "tittel": "Vedtak barnebidrag",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "tema": "BID",
+                  "tittel": "Barnebidrag, Saksbehandlingsnotat (fra bidragsmottaker sak 2500056)",
+                  "datoSortering": "2025-06-27T09:11:46",
+                  "journalpostId": "453999556",
+                  "mottaker": {
+                    "navn": "Selvhjulpen Mandag"
+                  },
+                  "avsender": null,
+                  "dokumenter": [
+                    {
+                      "dokumentInfoId": "454407507",
+                      "tittel": "Barnebidrag, Saksbehandlingsnotat (fra bidragsmottaker sak 2500056)",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "tema": "BID",
+                  "tittel": "Svar på forhåndsvarsel i sak om barnebidrag til bidragsmottaker",
+                  "datoSortering": "2025-06-24T11:17:17",
+                  "journalpostId": "453998552",
+                  "mottaker": {
+                    "navn": "Selvhjulpen Mandag"
+                  },
+                  "avsender": null,
+                  "dokumenter": [
+                    {
+                      "dokumentInfoId": "454406332",
+                      "tittel": "Svar på forhåndsvarsel i sak om barnebidrag til bidragsmottaker",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "tema": "BID",
+                  "tittel": "Bidragsforskudd, Saksbehandlingsnotat",
+                  "datoSortering": "2025-06-24T09:24:44",
+                  "journalpostId": "453998503",
+                  "mottaker": {
+                    "navn": "Selvhjulpen Mandag"
+                  },
+                  "avsender": null,
+                  "dokumenter": [
+                    {
+                      "dokumentInfoId": "454406259",
+                      "tittel": "Bidragsforskudd, Saksbehandlingsnotat",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    },
+                    {
+                      "dokumentInfoId": "454406260",
+                      "tittel": "WNIOSEK O DORĘCZENIE ZA GRANICĄ DOKUMENTÓW SĄDOWYCH I POZASĄDOWYCH (Forkynningsskjema (Polsk, Fransk, Engelsk))",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "tema": "BID",
+                  "tittel": "Søknadsskjema for bidragspliktig",
+                  "datoSortering": "2025-03-09T10:17:46",
+                  "journalpostId": "453969746",
+                  "mottaker": null,
+                  "avsender": {
+                    "navn": "SELVHJULPEN, MANDAG"
+                  },
+                  "dokumenter": [
+                    {
+                      "dokumentInfoId": "454373261",
+                      "tittel": "Søknadsskjema for bidragspliktig",
+                      "dokumentvarianter": [
+                        {
+                          "variantformat": "ARKIV"
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        }
+    """.trimIndent()
