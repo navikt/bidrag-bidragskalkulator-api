1073aa4c7c71fb8725f1630ccca9abeb23cab4f5
natchananthawisit <natcha.nanthawisit@nav.no>
2025-08-04 13:41:57 +0200

    Legg til sikkerhetskonfigurasjon og applikasjonskontekst i FoerstesidegeneratorConfig

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/config/FoerstesidegeneratorConfig.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/config/FoerstesidegeneratorConfig.kt
@@ -1,12 +1,17 @@
 package no.nav.bidrag.bidragskalkulator.config
 
 import no.nav.bidrag.bidragskalkulator.consumer.FoerstesidegeneratorConsumer
+import no.nav.bidrag.commons.security.api.EnableSecurityConfiguration
+import no.nav.bidrag.commons.service.AppContext
+import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import org.springframework.web.client.RestTemplate
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType.APPLICATION_JSON
@@ -14,6 +19,10 @@ import org.springframework.http.MediaType.APPLICATION_JSON
 @Configuration
 @ConfigurationPropertiesScan
 @EnableConfigurationProperties(value = [FoerstesidegeneratorConfigurationProperties::class])
+@EnableJwtTokenValidation
+@EnableOAuth2Client(cacheEnabled = true)
+@EnableSecurityConfiguration
+@Import(AppContext::class)
 class FoerstesidegeneratorConfig {
 
     @Bean
