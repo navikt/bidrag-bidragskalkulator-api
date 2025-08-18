3d589afd81e2629c2c53f156030981635e60cd56
Kristian Storvoll <kristian.storvoll@nav.no>
2025-08-18 10:29:55 +0200

    Opprydding

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/service/BidragKafkaEventService.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/service/BidragKafkaEventService.kt
@@ -1,6 +1,5 @@
 package no.nav.bidrag.bidragskalkulator.service
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.bidrag.commons.util.secureLogger
 import no.nav.bidrag.transport.sak.Sakshendelse
 import org.springframework.stereotype.Service
