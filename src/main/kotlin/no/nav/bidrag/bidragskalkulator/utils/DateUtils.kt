d9ed7d124bf1553540bc0f5ef370b177bf054226
natchananthawisit <natcha.nanthawisit@nav.no>
2025-08-28 10:08:35 +0200

    Standardiser logging ved å erstatte `secureLogger` med `logger` i `DateUtils` og oppdatere loggmeldinger.

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/utils/DateUtils.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/utils/DateUtils.kt
@@ -1,18 +1,17 @@
 package no.nav.bidrag.bidragskalkulator.utils
 
-
 import org.slf4j.LoggerFactory
 import java.time.LocalDate
 import java.time.Period
 import java.time.format.DateTimeParseException
 
-private val secureLogger = LoggerFactory.getLogger("secureLogger")
+private val logger = LoggerFactory.getLogger("DateUtils")
 
 fun kalkulerAlder(fødselsdato: LocalDate): Int {
     return try {
         Period.between(fødselsdato, LocalDate.now()).years
     } catch (e: DateTimeParseException) {
-        secureLogger.warn("Feil ved kalkulering av alder for fødselsdato $fødselsdato", e)
+        logger.warn("Feil ved kalkulering av alder for fødselsdato $fødselsdato", e)
         0
     }
 }
