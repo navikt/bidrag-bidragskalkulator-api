8fc98136a5d3534ec3d174785147daefa33037be
natchananthawisit <natcha.nanthawisit@nav.no>
2026-01-29 13:38:13 +0100

    Legger til inntektsgrenser for barn i `BidragskalkulatorGrunnlagDto`: Oppdaterer `hentGrunnlagsData` for å inkludere `barnInntektsgrense` og `selvforsørgetBarnInntektsgrense` basert på forskuddssats.

--- src/main/kotlin/no/nav/bidrag/bidragskalkulator/service/BidragskalkulatorGrunnlagService.kt
+++ src/main/kotlin/no/nav/bidrag/bidragskalkulator/service/BidragskalkulatorGrunnlagService.kt
@@ -1,19 +1,25 @@
 package no.nav.bidrag.bidragskalkulator.service
 
-import kotlinx.coroutines.coroutineScope
 import no.nav.bidrag.bidragskalkulator.dto.BidragskalkulatorGrunnlagDto
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 
+private const val ANTALL_FORSKUDDSSATSER_FOR_INNTEKTSPAVIRKNING = 30
+private const val ANTALL_FORSKUDDSSATSER_FOR_SELVFORSORGET_BARN = 100
 
 @Service
 class BidragskalkulatorGrunnlagService(
     private val sjablonService: SjablonService,
-    private val boOgForbruksutgiftService: BoOgForbruksutgiftService
+    private val boOgForbruksutgiftService: BoOgForbruksutgiftService,
 ) {
-    suspend fun hentGrunnlagsData(): BidragskalkulatorGrunnlagDto = coroutineScope {
-        BidragskalkulatorGrunnlagDto(
+    suspend fun hentGrunnlagsData(): BidragskalkulatorGrunnlagDto {
+        val forskuddssats = sjablonService.hentForskuddssats()
+
+        return BidragskalkulatorGrunnlagDto(
             boOgForbruksutgifter = boOgForbruksutgiftService.genererBoOgForbruksutgiftstabell(),
             samværsfradrag = sjablonService.hentSamværsfradrag(),
+            barnInntektsgrense = forskuddssats * BigDecimal(ANTALL_FORSKUDDSSATSER_FOR_INNTEKTSPAVIRKNING),
+            selvforsørgetBarnInntektsgrense = forskuddssats * BigDecimal(ANTALL_FORSKUDDSSATSER_FOR_SELVFORSORGET_BARN),
         )
     }
 }
